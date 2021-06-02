2b47ad9bf75c4719357bc4c95ca785138a04dd1b
epgfm <recon@vlay.org>
2021-06-02 09:57:25 +0200

    Refactored angel drawing into a function

--- 04-angelFig/js/app.js
+++ 04-angelFig/js/app.js
@@ -5,6 +5,23 @@ function drawLine(xa, ya, xb, yb) {
     ctx.lineTo(xb, yb);
 }
 
+function drawAngel(ctx, x, y, scale) {
+        ctx.beginPath();
+        ctx.strokeStyle = "#FFFFFFAA";
+        // Select target area
+        for (var i = 00; i <= 50*scale; i += 4) {
+            // elipse from center to a little above on the left
+            ctx.moveTo(x, y);
+            ctx.bezierCurveTo(x - 5*scale, y - 40*scale, x - 10*scale, y - 50*scale, x - 40*scale, y - 120*scale + i);
+            // Straight line to a bottom on the right
+            ctx.lineTo(x + 20*scale, y);
+            ctx.moveTo(x, y);
+            ctx.bezierCurveTo(x + 5*scale, y - 40*scale, x + 10*scale, y - 50*scale, x + 40*scale, y - 120*scale + i);
+            ctx.lineTo(x - 20*scale, y);
+            ctx.moveTo(x - 20*scale, y + 20*scale);
+        }
+        ctx.stroke();
+}
 
 function handleLoaded() {
     var startT = new Date();
@@ -13,24 +30,9 @@ function handleLoaded() {
     ctx.rect(0, 0, 1920, 1080);
     ctx.fillStyle = "black";
     ctx.fill();
-    ctx.beginPath();
-    ctx.strokeStyle = "#FFFFFFAA";
-    // Select target area
-    var midx = 1920 / 2;
-    var midy = 1080 / 2;
-    for (var i = 00; i <= 50; i += 4) {
-        // elipse from center to a little above on the left
-        ctx.moveTo(midx, midy);
-        ctx.bezierCurveTo(midx-5, midy-40, midx-10, midy-50, midx-40, midy-120+i);
-        // Straight line to a bottom on the right
-        ctx.lineTo(midx+20, midy);
-        ctx.moveTo(midx, midy);
-        ctx.bezierCurveTo(midx+5, midy-40, midx+10, midy-50, midx+40, midy-120+i);
-        ctx.lineTo(midx-20, midy);
-        ctx.moveTo(midx-20, midy+20);
-    }
-
-    ctx.stroke();
+    drawAngel(ctx, 1920 / 2, 1080 / 2 + 400, 5);
+    drawAngel(ctx, 1920 / 2, 1080 / 2, 2);
+    drawAngel(ctx, 1920 / 2, 1080 / 2 - 170, 0.7);
     var endT = new Date();
     var elapsed = endT - startT;
     console.log("Rendering took " + elapsed + "ms.");
